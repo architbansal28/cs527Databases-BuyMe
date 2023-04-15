d4c4422e26955444a6ec736ed9bf8ce1348a5afd
Archit Bansal <architbansal28@gmail.com>
2023-04-14 20:01:36 -0400

    update schema, add alert table

--- BuyMe.sql
+++ BuyMe.sql
@@ -93,6 +93,22 @@ CREATE TABLE `end_user` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Table structure for table `alert`
+--
+
+DROP TABLE IF EXISTS `alert`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alert` (
+  `end_user_id` varchar(20) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `message` varchar(5000) NOT NULL,
+  PRIMARY KEY (`end_user_id`,`message`),
+  FOREIGN KEY (`end_user_id`) REFERENCES `end_user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `question`
 --
 
@@ -262,7 +278,6 @@ CREATE TABLE `wishlist` (
   `cat_id` varchar(10) NOT NULL,
   `subcat_id` varchar(10) NOT NULL,
   `item_id` int NOT NULL,
-  `alert` bool NOT NULL DEFAULT 0,
   PRIMARY KEY (`user_id`,`cat_id`,`subcat_id`,`item_id`),
   FOREIGN KEY (`item_id`,`cat_id`,`subcat_id`) REFERENCES `item` (`item_id`,`cat_id`,`subcat_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   FOREIGN KEY (`user_id`) REFERENCES `end_user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
