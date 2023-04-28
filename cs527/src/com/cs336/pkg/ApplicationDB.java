bb97a29eaf52be3a778a78e523e73a74460bafea
Archit Bansal <architbansal28@gmail.com>
2023-04-28 11:59:00 -0400

    change database name for MAC OS export

--- cs527/src/com/cs336/pkg/ApplicationDB.java
+++ cs527/src/com/cs336/pkg/ApplicationDB.java
@@ -13,7 +13,7 @@ public class ApplicationDB {
 	public Connection getConnection(){
 		
 		//Create a connection string
-		String connectionUrl = "jdbc:mysql://localhost:3306/BuyMe";
+		String connectionUrl = "jdbc:mysql://localhost:3306/buyme";
 		Connection connection = null;
 		
 		try {
